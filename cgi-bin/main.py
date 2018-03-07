09b96fe1ef788e00861ca43988dcd7e0b54d0f73
Bob Claster <navchalkin@ex.ua>
2018-03-07 22:31:36 +0200

    fixed bug

--- cgi-bin/main.py
+++ cgi-bin/main.py
@@ -32,15 +32,15 @@ file = open('html/pattern.html', 'r')
 pattern = file.read()
 
 header = 'All operators'
-out ="""
-        <form action="/cgi-bin/main.py" style="display: inline; ">
+out ="""<h1 style="text-align: center;">Моніторинг</h1>
+        <!--<form action="/cgi-bin/main.py" style="display: inline; ">
             <input name="search" type="text" placeholder="Пошук">
             <button type="submit">Пошук</button>
-        </form>
+        </form>-->
         <hr>
-        
+
         <table>
-        
+
         <tr>
             <td align='center'><h2>Напруга (V)</h2></td>
             <td align='center'><h2>Струм (А)</h2></td>
@@ -55,10 +55,7 @@ for article in articles:
     out += "<td align='center'><h3>" + str(article[1]) + "</h3></td>"
     out += "<td align='center'><h3>" + str(article[2]) + "</h3></td>"
     out += "<td align='center'><h3>" + str(article[3]) + "</h3></td>"
-    out += "<td align='center'><h3>" + str(article[4]) + "</h3></td>"
-    out += "<td style='width: 100px;'>"
-
-    out += "</td>"
+    out += "<td align='center'><h3>" + str(article[4])[:19] + "</h3></td>"
     out += '</tr>'
 
 #           OUT
--- serv.py
+++ serv.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from http.server import HTTPServer, CGIHTTPRequestHandler
-server_address = ("", 8080)
+server_address = ("", 8199)
 httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
 httpd.serve_forever()
